6e13ce973b64abec217666c2bdd099004e98385e
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2023-01-07 17:48:35 -0400

    Menu Iterativo
    Criado um menu iterativo para cadastrar cliente, autor, livro e emprestar um livro

--- PraticandoTeste/Program.cs
+++ PraticandoTeste/Program.cs
@@ -15,23 +15,51 @@ namespace PraticandoTeste
             {
                 using (var banco = new Context())
                 {
-
-                    // AluguelLivro(2, banco);
-
-                    ConsultarLivrosAlugados(banco, 1);
+                    Menu(banco);
                 }
-
-
-                //Console.WriteLine("Adcionado com sucesso");
-              
             }
-            catch (SqlException ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
 
             Console.ReadKey();
         }
+        static void Menu(Context banco)
+        {
+            while (true)
+            {
+                banco.SaveChanges();
+                Console.Clear();
+                Console.WriteLine("1-Cadastrar Autor\n2-Cadastrar Livro\n3-Cadastrar Cliente\n4-Emprestar Livro\n0-Sair\nOpcao: ");
+                var opc = Console.ReadLine();
+
+                switch (opc)
+                {
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    case "1":
+                        AdcionarAutor(banco);
+                        break;
+                    case "2":
+                        AdcionarLivro(banco);
+                        break;
+                        case "3":
+                        AdcionarCliente(banco);
+                        break;
+                    case "4":
+                        AluguelLivro(banco);
+                        break;
+                    default:
+                        Console.WriteLine("Opcao Incorreta");
+                        break;
+                }
+                Console.ReadKey();
+
+            }
+        }
+       
         static void ConsultarLivrosAlugados(Context banco, int idCliente)
         {
             var cliente = banco.Clientes.Where(x => x.Id == idCliente).Include(x => x.Livros).AsNoTracking().ToList();
@@ -45,45 +73,72 @@ namespace PraticandoTeste
 
                 ).AsNoTracking().ToList();
 
+
             var serializarJson = JsonConvert.SerializeObject(livroAlugado, Formatting.Indented);
 
             Console.WriteLine(serializarJson);
         }
-        static void AluguelLivro(int idLivro, Context banco)
+        static void AluguelLivro(Context banco)
         {
-           var livro = banco.Livros.FirstOrDefault(x => x.Id.Equals(idLivro));
-            var cliente = banco.Clientes.FirstOrDefault(x => x.Id.Equals(18));
-            if (livro is null || cliente is null)
+            Console.WriteLine("Informe o Id do Cliente: ");
+            var cliente = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o Id do livro: ");
+            var idLivro = int.Parse(Console.ReadLine());
+
+            var clienteBanco = banco.Clientes.FirstOrDefault(x => x.Id == cliente);
+            var livroBanco = banco.Livros.FirstOrDefault(x => x.Id == idLivro);
+
+            Console.WriteLine(clienteBanco.Nome);
+            Console.WriteLine(livroBanco.Nome);
+            Console.WriteLine("Confirma, enter para continuar");
+            Console.ReadKey();
+
+            clienteBanco.Livros = new List<Livro>() { livroBanco };
+
+            banco.Update(clienteBanco);
+            Console.WriteLine("Alugado com sucesso");
+        }
+        static void AdcionarCliente(Context banco)
+        {
+            Console.WriteLine("Nome Cliente: ");
+            string nomeCliente = Console.ReadLine();
+
+            banco.Clientes.Add(new Cliente { Nome = nomeCliente});
+            Console.WriteLine("Cadastrado com sucesso");
+        }
+        static void AdcionarLivro(Context banco)
+        {
+            Console.WriteLine("Nome Livro: ");
+            string nomeLivro = Console.ReadLine();
+            Console.WriteLine("Quantidade de páginas: ");
+            int qtdPaginas = int.Parse(Console.ReadLine());
+            Console.WriteLine("------------------");
+            foreach (var item in banco.Autores)
+            {
+                Console.WriteLine(item.Id + " " + item.Nome);
+            }
+            Console.WriteLine("------------------");
+            Console.WriteLine("Informe o Id do autor: ");
+            int idAutor = int.Parse(Console.ReadLine());
+            var autor = banco.Autores.FirstOrDefault(x => x.Id == idAutor);
+
+            if (autor is null)
                 return;
 
-            cliente.Livros = new List<Livro>() { livro };
-
-            banco.Update(cliente);
-            banco.SaveChanges();
+            banco.Livros.Add(new Livro
+            {
+                Nome = nomeLivro,
+                QuantidadeDePaginas = qtdPaginas,
+                Autor = autor
+            });
+            Console.WriteLine("Cadastrado com sucesso");
         }
-        static Cliente AdcionarCliente(string nome)
+        static void AdcionarAutor(Context banco)
         {
-            var cliente = new Cliente();
-            cliente.Nome = nome;
-
-            return cliente;
-        }
-        static Livro AdcionarLivro(string nomeLivro, int quantidadePaginas, Autor autor)
-        { 
-            var livro = new Livro();
-            livro.Nome= nomeLivro;
-            livro.QuantidadeDePaginas = quantidadePaginas;
-            livro.Autor = autor;
-
-
-            return livro;
-        }
-        static Autor AdcionarAutor(string nome)
-        {
-            var autor = new Autor();
-            autor.Nome = nome;
-
-            return autor;
+            Console.WriteLine("Nome Autor: ");
+            string nome = Console.ReadLine();
+            banco.Autores.Add(new Autor { Nome = nome});
+            Console.WriteLine("Cadastrado com sucesso");
         }
     }
 }
